24211e37be2838d38d2b72979669b9f995731612
MajdFoudeh <majdfoude@gmail.com>
2023-06-16 23:10:05 +0300

    profile

--- Front-end/src/components/userprofile/orderhistory.jsx
+++ Front-end/src/components/userprofile/orderhistory.jsx
@@ -21,7 +21,7 @@ export default function Orderhistory() {
                                     <h3 className="text-xl xl:text-2xl font-semibold leading-6 text-gray-800">Premium Quaility Dress</h3>
                                     <div className="flex justify-start items-start flex-col space-y-2">
                                         <p className="text-sm leading-none text-gray-800">
-                                            <span className="text-gray-300">Style: </span> Italic Minimal Design
+                                            <span className="text-gray-300">Event name: </span> Italic Minimal Design
                                         </p>
                                         <p className="text-sm leading-none text-gray-800">
                                             <span className="text-gray-300">Size: </span> Small
@@ -50,7 +50,7 @@ export default function Orderhistory() {
                                     <h3 className="text-xl xl:text-2xl font-semibold leading-6 text-gray-800">High Quaility Italic Dress</h3>
                                     <div className="flex justify-start items-start flex-col space-y-2">
                                         <p className="text-sm leading-none text-gray-800">
-                                            <span className="text-gray-300">Style: </span> Italic Minimal Design
+                                            <span className="text-gray-300">Event name: </span> Italic Minimal Design
                                         </p>
                                         <p className="text-sm leading-none text-gray-800">
                                             <span className="text-gray-300">Size: </span> Small
@@ -61,11 +61,14 @@ export default function Orderhistory() {
                                     </div>
                                 </div>
                                 <div className="flex justify-between space-x-8 items-start w-full">
-                                    <p className="text-base xl:text-lg leading-6">
+                                    {/* <p className="text-base xl:text-lg leading-6">
                                         $20.00 <span className="text-red-300 line-through"> $30.00</span>
-                                    </p>
+                                    </p> */}
                                     <p className="text-base xl:text-lg leading-6 text-gray-800">01</p>
                                     <p className="text-base xl:text-lg font-semibold leading-6 text-gray-800">$20.00</p>
+                                    <button class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded">
+                                        View recipet
+                                    </button>
                                 </div>
                             </div>
                         </div>
