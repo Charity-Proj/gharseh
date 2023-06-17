8b2aa863b9e83e08d72e25faa252d622edf2fd8a
abdalrahman-droubi <abdalrahman.drobi@outlook.com>
2023-06-17 13:03:48 +0300

    create branch

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import './App.css'
 import Layout from './layout/layout';
